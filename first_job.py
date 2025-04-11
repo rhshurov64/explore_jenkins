b0a61ea3d3f5f97264ca0947da6b9fae457403dc
Md. Rakibul Hasan <42247328+rhshurov64@users.noreply.github.com>
2025-04-11 11:15:09 +0600

    Create first_job.py
    first job file,

---
+++ first_job.py
@@ -0,0 +1,8 @@
+# First github job for jenkins
+# Schedule jenkins to run this file on a specific time.
+# Email Notification to the dev email when error comes.
+
+x = "Hello World!"
+y = "This is my first job."
+z = f"{x}, {y}"
+print(z)
